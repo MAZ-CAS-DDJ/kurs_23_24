5423494ed14b22d32a3884523e98f6081d84a739
Thomas Ebermann <thomas.ebermann@liip.ch>
2023-10-25 14:42:12 +0200

    update

--- 13 Github Actions/scrape_winterthurer_zeitung.py
+++ 13 Github Actions/scrape_winterthurer_zeitung.py
@@ -6,12 +6,14 @@ import requests
 from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
-    html_content = requests.get("https://84xo.ch").text
-    soup = BeautifulSoup(html_content)
-    main_headline = soup.select(".MainHighlightedNews_main_highlighted_news_title__uFcsH")[0].text
+    html_content = requests.get("https://www.winterthurer-zeitung.ch/").text
+    soup = BeautifulSoup(html_content,"lxml")
+    main_headline = soup.select(".article.articletype-0 h2 a")[0].text.strip()
     main_headline = "\n- " + datetime.datetime.now().strftime("%H:%M %d.%m.%y") + " " + main_headline
-    readme = Path("README.md").read_text(encoding="utf8")
+    readme = Path("SCRAPE.md").read_text(encoding="utf8")
     readme_title = "# Latest 84xo Headlines"
     new_readme = readme[:readme.find(readme_title)] + f"{main_headline}\n\n"
-    with open("README.md", "w+") as f:
+    with open("SCRAPE.md", "w+") as f:
         f.write(new_readme)
+
+
